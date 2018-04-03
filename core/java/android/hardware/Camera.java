f080fffc4312277c410af8e9aa132b72e987612a
Preeti Nagar <pnagar@codeaurora.org>
2018-04-03 16:34:25 +0530

    SEEMP: framework instrumentation and AppProtect features
    - Changes for Android-P porting
    - Instrument framework files to log events of interest for QSSP.
    - Fix the String out of boundary.
    - Add optional package verifier to PackageManagerService
      - In additional to the required and sufficient package verifiers,
        add support for an optional verifier in PackageManager. The
        optional verifier will act as a required verifier if a required
        verifier is not found in the system. It will act as a secondary
        verifier if a required verifier is present.
    - Add auth framework for outgoing SMS messages.
      - Add the APIs required to register an authorization agent in order
        to allow/reject outgoing SMS messages and the core service that
        implements the sms security model.
    - Do not connect to seempdw socket in zygote
      - Do not connect to seempdw socket in zygote. The socket
        initialization is skipped if the parent process id is 1, which is
        always the case for zygote.
    - Moved instrumentation point to PackageInstaller and some
      minor fixes.
    - Add action for optional package verifier along other legacy
      actions to handle tightened StrictMode policy in Android P.
    
    Change-Id: I38a8ace9943fae0ad2ac7bc2932f3deb790359a6
    CRs-Fixed: 2219016
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -909,6 +909,7 @@ public class Camera {
      * @see android.media.MediaActionSound
      */
     public final void setPreviewCallback(PreviewCallback cb) {
+        android.util.SeempLog.record(66);
         mPreviewCallback = cb;
         mOneShot = false;
         mWithBuffer = false;
@@ -937,6 +938,7 @@ public class Camera {
      * @see android.media.MediaActionSound
      */
     public final void setOneShotPreviewCallback(PreviewCallback cb) {
+        android.util.SeempLog.record(68);
         mPreviewCallback = cb;
         mOneShot = true;
         mWithBuffer = false;
@@ -977,6 +979,7 @@ public class Camera {
      * @see android.media.MediaActionSound
      */
     public final void setPreviewCallbackWithBuffer(PreviewCallback cb) {
+        android.util.SeempLog.record(67);
         mPreviewCallback = cb;
         mOneShot = false;
         mWithBuffer = true;
@@ -1558,6 +1561,7 @@ public class Camera {
      */
     public final void takePicture(ShutterCallback shutter, PictureCallback raw,
             PictureCallback jpeg) {
+        android.util.SeempLog.record(65);
         takePicture(shutter, raw, null, jpeg);
     }
     private native final void native_takePicture(int msgType);
@@ -1596,6 +1600,7 @@ public class Camera {
      */
     public final void takePicture(ShutterCallback shutter, PictureCallback raw,
             PictureCallback postview, PictureCallback jpeg) {
+        android.util.SeempLog.record(65);
         mShutterCallback = shutter;
         mRawImageCallback = raw;
         mPostviewCallback = postview;
