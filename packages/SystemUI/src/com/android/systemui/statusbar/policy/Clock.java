67a79f583765e91cbc0975e7e3b0f773bc396806
maxwen <max.weninger@gmail.com>
2018-09-13 14:47:41 +0200

    base: SystemUI: tuner: fix hiding of statusbar clock
    clock in expanded quick settings will not be hidden
    from tuner setting
    
    Change-Id: Ic5db96fba79e5bc77282b70398e7f8055b998892
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -81,6 +81,7 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
 
     private boolean mClockVisibleByPolicy = true;
     private boolean mClockVisibleByUser = true;
+    private boolean mClockHideableByUser = true;
 
     private boolean mAttached;
     private Calendar mCalendar;
@@ -270,8 +271,10 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
     }
 
     public void setClockVisibleByUser(boolean visible) {
-        mClockVisibleByUser = visible;
-        updateClockVisibility();
+        if (mClockHideableByUser) {
+            mClockVisibleByUser = visible;
+            updateClockVisibility();
+        }
     }
 
     public void setClockVisibilityByPolicy(boolean visible) {
@@ -287,6 +290,14 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
         boolean visible = shouldBeVisible();
         int visibility = visible ? View.VISIBLE : View.GONE;
         super.setVisibility(visibility);
+    }
+
+    public boolean isClockVisible() {
+        return mClockVisibleByPolicy && mClockVisibleByUser;
+    }
+
+    public void setClockHideableByUser(boolean value) {
+        mClockHideableByUser = value;
     }
 
     final void updateClock() {
