2eed9241757a43ba11e795e370b52d19d0a80ce7
Pranav Vashi <neobuddy89@gmail.com>
2020-01-18 05:56:05 -0500

    base: Add Samsung One font [1/2]
    Change-Id: Iad1ec93aaa83d4b1e8ed792502b08e0c4853fe70
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -38,6 +38,7 @@ LOCAL_REQUIRED_MODULES := \
 	FontGoogleSansOverlay \
 	FontNotoSerifSourceOverlay \
 	FontRosemarySourceOverlay \
+    FontSamsungOneSourceOverlay \
 	FontSFProOverlay \
 	FontSonySketchSourceOverlay \
 	FontSurferSourceOverlay \
