a07d62445b79411bd035bbbf6a6d87db3a660f7e
Anushek Prasal <anushekprasal@gmail.com>
2020-06-28 23:27:20 +0530

    base: Fix navbar when IME space is hidden
    aosip edit:
    switch to using our isGestureNavbar util instead of individual overlays
    
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Change-Id: Ie0feff79f287a11b75490fc175a522f036feb41b

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -2702,9 +2702,9 @@ public class DisplayPolicy {
                 res.getDimensionPixelSize(R.dimen.navigation_bar_height) : 0;
         final int navbarHeightLandscape = showNavbar ?
                 res.getDimensionPixelSize(R.dimen.navigation_bar_height_landscape) : 0;
-        final int navbarFrameHeight = showIMESpace ?
+        final int navbarFrameHeight = showIMESpace || !gestureNavbarHidden() ?
                 res.getDimensionPixelSize(R.dimen.navigation_bar_frame_height) : 0;
-        final int navbarFrameHeightLandscape = showIMESpace ?
+        final int navbarFrameHeightLandscape = showIMESpace || !gestureNavbarHidden() ?
                 res.getDimensionPixelSize(R.dimen.navigation_bar_frame_height_landscape) : 0;
         final int navbarWidth = showNavbar ?
                 res.getDimensionPixelSize(R.dimen.navigation_bar_width) : 0;
@@ -3797,5 +3797,12 @@ public class DisplayPolicy {
         final WindowManager wm = mContext.getSystemService(WindowManager.class);
         wm.removeView(mPointerLocationView);
         mPointerLocationView = null;
+    }
+
+    private boolean gestureNavbarHidden() {
+        boolean isGestureNavbar = (derpUtils.isGestureNavbar());
+        boolean isNavbarHidden = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.NAVIGATION_HANDLE_WIDTH, 1) == 0;
+        return isGestureNavbar && isNavbarHidden;
     }
 }
